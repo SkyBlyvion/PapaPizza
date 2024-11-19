9fc57bb629f3f711e7610e28b8cdd02bb92ec03b
SkyBlyvion <richtofen60@gmail.com>
2024-11-19 11:28:53 +0100

    testcommit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Lancer le projet
+- cloner le projet
+- installer Lando
+- console : lando start
+- ouvrir le projet dans un navigateur
+- console : lando composer install
+- lando mysql
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
+        'reference' => '6447f2e84f7abb1a592154c084465ec556b9284f',
         'name' => '__root__',
         'dev' => true,
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => '6447f2e84f7abb1a592154c084465ec556b9284f',
             'dev_requirement' => false,
         ),
         'laminas/laminas-diactoros' => array(
